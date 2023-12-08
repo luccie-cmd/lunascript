5cc2f5640bed63551ee8400f4c1db9c753785e38
luccie-cmd <hoedje201@gmail.com>
2023-12-08 13:26:06 +0100

    Make DIAG a class

--- include/diag.hh
+++ include/diag.hh
@@ -3,47 +3,50 @@
 #include <fmt/core.h>
 
 namespace luna{
-    struct Diag{
-    enum Color {
-        Reset = 0,
-        Red = 31,
-        Green = 32,
-        Yellow = 33,
-        Blue = 34,
-        Magenta = 35,
-        Cyan = 36,
-        White = 37,
-    };
-    std::string string_from_color(Color c);
-    bool use_color = true;
-    bool exit_on_error = true;
-    Color info_color = Color::Reset;
-    Color note_color = Color::Reset;
-    Color warning_color = Color::Magenta;
-    Color error_color = Color::Red;
-    void init(bool color = true, bool exit = true);
-    template <typename... Args>
-    void Info(fmt::format_string<Args...> fmt, Args&&... args){
-        fmt::print("{}INFO: {}{}", string_from_color(info_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
-    }
-    template <typename... Args>
-    void Note(fmt::format_string<Args...> fmt, Args&&... args){
-        fmt::print("{}NOTE: {}{}", string_from_color(note_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
-    }
-    template <typename... Args>
-    void Warning(fmt::format_string<Args...> fmt, Args&&... args){
-        fmt::print("{}WARNING: {}{}", string_from_color(warning_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
-    }
-    template <typename... Args>
-    void Error(fmt::format_string<Args...> fmt, Args&&... args){
-        fmt::print("{}ERROR: {}{}", string_from_color(error_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
-        if(exit_on_error) std::exit(1);
-    }
-    template <typename... Args>
-    void ICE(fmt::format_string<Args...> fmt, Args&&... args){
-        fmt::print("{}Internal Compiler Error: {}{}", string_from_color(error_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
-        if(exit_on_error) std::exit(1);
-    }
+    class Diag{
+    private:
+        enum Color {
+            Reset = 0,
+            Red = 31,
+            Green = 32,
+            Yellow = 33,
+            Blue = 34,
+            Magenta = 35,
+            Cyan = 36,
+            White = 37,
+        };
+        bool use_color = true;
+        bool exit_on_error = true;
+        Color info_color = Color::Reset;
+        Color note_color = Color::Reset;
+        Color warning_color = Color::Magenta;
+        Color error_color = Color::Red;
+        std::string string_from_color(Color c);
+
+    public:
+        void init(bool color = true, bool exit = true);
+        template <typename... Args>
+        void Info(fmt::format_string<Args...> fmt, Args&&... args){
+            fmt::print("{}INFO: {}{}", string_from_color(info_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
+        }
+        template <typename... Args>
+        void Note(fmt::format_string<Args...> fmt, Args&&... args){
+            fmt::print("{}NOTE: {}{}", string_from_color(note_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
+        }
+        template <typename... Args>
+        void Warning(fmt::format_string<Args...> fmt, Args&&... args){
+            fmt::print("{}WARNING: {}{}", string_from_color(warning_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
+        }
+        template <typename... Args>
+        void Error(fmt::format_string<Args...> fmt, Args&&... args){
+            fmt::print("{}ERROR: {}{}", string_from_color(error_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
+            if(exit_on_error) std::exit(1);
+        }
+        template <typename... Args>
+        void ICE(fmt::format_string<Args...> fmt, Args&&... args){
+            fmt::print("{}Internal Compiler Error: {}{}", string_from_color(error_color), fmt::format(fmt, std::forward<Args>(args)...), string_from_color(Color::Reset));
+            if(exit_on_error) std::exit(1);
+        }
 };
 };
 
